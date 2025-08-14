14ea2868775c0b3ca6665aef48710aed78ccf763
Vinaigrette0403 <lifehot2@naver.com>
2025-08-14 15:04:28 +0900

    Lv.0

---
+++ src/LV_0/문자출력.java
@@ -0,0 +1,12 @@
+package LV_0;
+
+	public class 문자출력 {
+	    public static void main(String[] args) {
+	        String message = "Let's go!";
+
+	        System.out.println("3\n2\n1");
+	        System.out.println(message);
+	    }
+	}
+
+
